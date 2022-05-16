3563968c0f4905edd1c1514e85913b80c3d6e716
vijayrjdev@gmail.com <vijayrajck@outlook.com>
2022-05-16 09:51:05 +0530

    Initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Loader from "./components/Loader";
+import Services from "./components/Services";
+import Transactions from "./components/Transactions";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
